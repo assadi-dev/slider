7ca4155d0913a4d22123c5887fc7145d93b9e70f
Assadi <assadi.halifa@gmail.com>
2022-01-07 22:35:04 +0100

    add  next and prev function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,29 @@ const indicators = document.querySelectorAll(".cercle-indicators");
 
 let index = 0;
 
-const slideSuivant = () => {
-    if (index < 2) {
+const nextSlide = () => {
+    if (index < images.length - 1) {
         images[index].classList.remove("active");
         index++;
+        images[index].classList.add("active");
+    } else {
+        images[index].classList.remove("active");
+        index = 0;
         images[index].classList.add("active");
     }
 };
 
-next.addEventListener("click", slideSuivant);
+const prevSlide = () => {
+    if (index > 0) {
+        images[index].classList.remove("active");
+        index--;
+        images[index].classList.add("active");
+    } else if (index === 0) {
+        images[index].classList.remove("active");
+        index = images.length - 1;
+        images[index].classList.add("active");
+    }
+};
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
